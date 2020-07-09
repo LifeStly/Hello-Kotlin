145c7fc582c118298f205f2cc618b06771340624
LifeStly <45162527+LifeStly@users.noreply.github.com>
2020-07-09 15:10:55 +0700

    practice time

--- mmm.kts
+++ mmm.kts
@@ -1,20 +1,10 @@
-fun getBirthday(): Int {
-    print("\nEnter your birthday: ")
-    return readLine()?.toIntOrNull() ?: 1
+fun main(args: Array<String>) {
+    println(whatShouldIDoToday("happy"))
 }
 
-fun getFortune(birthday: Int): String {
-    val fortunes = listOf("You will have a great day!",
-            "Things will go well for you today.",
-            "Enjoy a wonderful day of success.",
-            "Be humble and all will turn out well.",
-            "Today is a good day for exercising restraint.",
-            "Take it easy and enjoy life!",
-            "Treasure your friends, because they are your greatest fortune.")
-    val index = when (birthday) {
-        in 1..7 -> 4
-        28, 31 -> 2
-        else -> birthday.rem(fortunes.size)
+fun whatShouldIDoToday(mood: String, weather: String = "sunny", temperature: Int = 24) : String {
+    return when {
+        mood == "happy" && weather == "Sunny" -> "go for a walk"
+        else -> "Stay home and read."
     }
-    return fortunes[index]
 }
