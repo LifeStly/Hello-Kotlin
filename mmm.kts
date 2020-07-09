f8ce0935d22b55f3982da41c9b607e1b350fe8a7
LifeStly <45162527+LifeStly@users.noreply.github.com>
2020-07-09 15:03:20 +0700

    Initial commit

---
+++ mmm.kts
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    var fortune: String
+    for (i in 1..10) {
+        fortune = getFortuneCookie()
+        println("\nYour fortune is: $fortune")
+        if (fortune.contains("Take it easy")) break
+    }
+}
