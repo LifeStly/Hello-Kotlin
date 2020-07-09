1039ad6be0ab45f23c6002237bae71ad86f9576f
LifeStly <45162527+LifeStly@users.noreply.github.com>
2020-07-09 15:16:37 +0700

    save

--- mmm.kts
+++ mmm.kts
@@ -1,7 +1,2 @@
-fun main(args: Array<String>) {
-    var fortune: String = ""
-    while (!fortune.contains("Take it easy")) {
-        fortune = getFortune(getBirthday())
-        println("\nYour fortune is: $fortune")
-    }
-}
+spices.take(3).filter{it.startsWith('c')}
+> [curry, cayenne]
