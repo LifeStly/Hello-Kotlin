515530850f30cab0f40378de08c369067c20b582
LifeStly <45162527+LifeStly@users.noreply.github.com>
2020-07-09 15:06:47 +0700

    Starter

--- mmm.kts
+++ mmm.kts
@@ -1,8 +1,8 @@
 fun main(args: Array<String>) {
     var fortune: String
     for (i in 1..10) {
-        fortune = getFortuneCookie()
+        fortune = getFortune(getBirthday())
         println("\nYour fortune is: $fortune")
-        if (fortune.contains("Take it easy")) break
+        if (fortune.contains("Take it easy")) break;
     }
 }
