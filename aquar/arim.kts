c9dd752f6d3952df4cf09bdd8e1e624476e7e2ca
LifeStly <45162527+LifeStly@users.noreply.github.com>
2020-07-18 22:47:37 +0700

    save

--- aquar/arim.kts
+++ aquar/arim.kts
@@ -1,21 +1,3 @@
-class Curry(name: String, spiciness: String,
-            color: SpiceColor = YellowSpiceColor) : Spice(name, spiciness, color), Grinder {
-    override fun grind() {
-    }
-
-    override fun prepareSpice() {
-        grind()
-    }
-}
-
-interface Grinder {
-    fun grind()
-}
-
-interface SpiceColor {
-    val color: String
-}
-
-object YellowSpiceColor : SpiceColor {
-    override val color = "Yellow"
+data class SpiceContainer(var spice: Spice) {
+    val label = spice.name
 }
